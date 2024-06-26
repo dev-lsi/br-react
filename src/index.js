1e98f665bacd5ceaa7c3649409b9854a1cef9a14
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-06-27 02:40:54 +0300

    router initialized

--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Link,
+} from "react-router-dom";
+import logo from "./logo.svg";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Home = () => {
+  return <h1>Home</h1>;
+};
+const About = () => {
+  return <h1>About</h1>;
+};
+const Contact = () => {
+  return <h1>Contact</h1>;
+};
+
+const appRouter = createBrowserRouter([
+  { path: "/", element: <App />, children:[
+    {path:"/",element:<Home/>},
+    { path: "/contact", element: <Contact /> },
+    { path: "/about", element: <About /> },
+  ] }
+  
+]);
+
+const Main = () => {
+  return <Outlet />;
+};
+
+export const Header = () => {
+  return (
+    <header className="App-header">
+      <Link className="link-component" to={"/"}>Home-link</Link>
+      <Link className="link-component" to={"/contact"}>Contact-link</Link>
+      <Link className="link-component" to={"/about"}>About-link</Link>
+    </header>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={appRouter}/>
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
