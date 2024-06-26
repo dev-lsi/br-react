3bd6dee3927ea87a27ee3160a3b3609e64f02328
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-06-26 22:52:59 +0300

    my control branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload new data.
+          Edit <code>src/App.js</code> and save to reload new branch data.
         </p>
         <a
           className="App-link"
